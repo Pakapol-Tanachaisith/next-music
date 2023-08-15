78b54365425b66d8afce577b99c84872cfe7135e
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2023-08-15 13:00:56 +0700

    build like feature

--- components/song-card.tsx
+++ components/song-card.tsx
@@ -6,6 +6,7 @@ import { PlayCircle, Heart } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { LikeButton } from "./like-button";
 
 interface SongCardProps {
   song: Song;
@@ -38,12 +39,7 @@ export const SongCard = ({ song }: SongCardProps) => {
           )}
         </div>
         <div className="mt-3 text-right">
-          <Button
-            size="icon"
-            variant="ghost"
-          >
-            <Heart className="w-5 h-5 text-muted-foreground" />
-          </Button>
+          <LikeButton song={song} />
           <Button
             size="icon"
             variant="ghost"
