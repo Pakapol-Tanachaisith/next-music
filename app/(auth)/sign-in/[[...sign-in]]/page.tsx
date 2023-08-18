00ac9f907075031125ed36e25e9a82944d6ec529
Pakapol Tanachaisith <pakapol.tanachaisith@gmail.com>
2023-08-18 15:01:15 +0700

    add metadata to signin and signup pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import { SignIn } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+  title: "Next Music | Sign In",
+  description: "Sign in to your Next Music account.",
+};
 
 export default function Page() {
   return <SignIn />;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 import { SignUp } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+  title: "Next Music | Sign Up",
+  description: "Create new Next Music account.",
+};
 
 export default function Page() {
   return <SignUp />;
